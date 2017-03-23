3900fd03bbce270c95aff3d15fb1430f7bf58f19
Mathieu Briand <mbriand@witekio.com>
2017-03-23 10:51:34 +0100

    core: arm: Rework i.MX6 source files
    Add a CFG_MX6xxx variable for every i.MX6 SoC variants and get register
    addresses and values based on these variables instead of relying on
    board names.
    
    Signed-off-by: Mathieu Briand <mbriand@witekio.com>

--- core/arch/arm/plat-imx/conf.mk
+++ core/arch/arm/plat-imx/conf.mk
@@ -1,11 +1,20 @@
 PLATFORM_FLAVOR ?= mx6ulevk
 
-ifeq ($(PLATFORM_FLAVOR),mx6ulevk)
-arm32-platform-cpuarch		:= cortex-a7
+# Get SoC associated with the PLATFORM_FLAVOR
+ifeq ($(PLATFORM_FLAVOR),$(filter $(PLATFORM_FLAVOR),mx6ulevk))
+$(call force,CFG_MX6UL,y)
 endif
-ifeq ($(PLATFORM_FLAVOR),$(filter $(PLATFORM_FLAVOR),mx6qsabrelite mx6qsabresd mx6dlsabresd))
-arm32-platform-cpuarch		:= cortex-a9
+
+ifeq ($(PLATFORM_FLAVOR),$(filter $(PLATFORM_FLAVOR),mx6qsabrelite mx6qsabresd))
+$(call force,CFG_MX6Q,y)
 endif
+
+ifeq ($(PLATFORM_FLAVOR),$(filter $(PLATFORM_FLAVOR),mx6dlsabresd))
+$(call force,CFG_MX6DL,y)
+endif
+
+
+# Common i.MX6 config
 arm32-platform-cflags		+= -mcpu=$(arm32-platform-cpuarch)
 arm32-platform-aflags		+= -mcpu=$(arm32-platform-cpuarch)
 core_arm32-platform-aflags	+= -mfpu=neon
@@ -16,10 +25,23 @@ $(call force,CFG_GIC,y)
 $(call force,CFG_IMX_UART,y)
 $(call force,CFG_PM_STUBS,y)
 $(call force,CFG_WITH_SOFTWARE_PRNG,y)
-ifeq ($(PLATFORM_FLAVOR),mx6ulevk)
+
+CFG_CRYPTO_SIZE_OPTIMIZATION ?= n
+CFG_WITH_STACK_CANARIES ?= y
+
+
+# i.MX6UL specific config
+ifeq ($(filter y, $(CFG_MX6UL)), y)
+arm32-platform-cpuarch		:= cortex-a7
+
 $(call force,CFG_SECURE_TIME_SOURCE_CNTPCT,y)
 endif
-ifeq ($(PLATFORM_FLAVOR),$(filter $(PLATFORM_FLAVOR),mx6qsabrelite mx6qsabresd mx6dlsabresd))
+
+
+# i.MX6 Solo/DualLite/Dual/Quad specific config
+ifeq ($(filter y, $(CFG_MX6Q) $(CFG_MX6D) $(CFG_MX6DL) $(CFG_MX6S)), y)
+arm32-platform-cpuarch		:= cortex-a9
+
 $(call force,CFG_PL310,y)
 $(call force,CFG_PL310_LOCKED,y)
 $(call force,CFG_SECURE_TIME_SOURCE_REE,y)
@@ -28,7 +50,6 @@ CFG_BOOT_SYNC_CPU ?= y
 CFG_BOOT_SECONDARY_REQUEST ?= y
 endif
 
+
 ta-targets = ta_arm32
 
-CFG_CRYPTO_SIZE_OPTIMIZATION ?= n
-CFG_WITH_STACK_CANARIES ?= y
--- core/arch/arm/plat-imx/sub.mk
+++ core/arch/arm/plat-imx/sub.mk
@@ -4,7 +4,13 @@ srcs-y += main.c
 srcs-$(CFG_PL310) += imx_pl310.c
 srcs-$(CFG_PSCI_ARM32) += psci.c
 
-srcs-$(PLATFORM_FLAVOR_mx6qsabrelite) += a9_plat_init.S
-srcs-$(PLATFORM_FLAVOR_mx6qsabresd) += a9_plat_init.S
-srcs-$(PLATFORM_FLAVOR_mx6dlsabresd) += a9_plat_init.S
-srcs-$(PLATFORM_FLAVOR_mx6ulevk) += imx6ul.c
+srcs-$(CFG_MX6Q) += a9_plat_init.S
+srcs-$(CFG_MX6Q) += imx6.c
+srcs-$(CFG_MX6D) += a9_plat_init.S
+srcs-$(CFG_MX6D) += imx6.c
+srcs-$(CFG_MX6DL) += a9_plat_init.S
+srcs-$(CFG_MX6DL) += imx6.c
+srcs-$(CFG_MX6S) += a9_plat_init.S
+srcs-$(CFG_MX6S) += imx6.c
+
+srcs-$(CFG_MX6UL) += imx6ul.c
